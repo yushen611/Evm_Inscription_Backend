dca4c32777981ebeafb2984d12c4977ca1fde75f
yushen611 <460687076@qq.com>
2024-01-31 13:42:32 +0800

    problem

--- main_test.py
+++ main_test.py
@@ -1,15 +1,14 @@
 
 from pkg.eventGetter.EventsGetter import get_events
-
+from pkg.token_getter.tokens import *
 def testEvent():
     data = get_events()
     for e in data:
         print(e)
 
 def testJson():
-    ...
+    ntf_list, deployFT_map, ft_account_map  = get_valid_tokens()
+    print(deployFT_map)
 
 if __name__=="__main__" :
-    data = get_events()
-    for e in data:
-        print(e)
+    testEvent()
