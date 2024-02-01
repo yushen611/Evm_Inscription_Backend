aa3e97baf57a0110b3933b2fbdd6f0e95ad3d271
yushen611 <460687076@qq.com>
2024-02-01 19:13:31 +0800

    fast mode

--- pkg/eventGetter/EventsGetter.py
+++ pkg/eventGetter/EventsGetter.py
@@ -3,14 +3,20 @@ from responce.Response import *
 import requests
 from config.config import *
 import pkg.eventGetter.Event as e
+from  pkg.save_load.save_load import save_from_block,load_from_block
+from pkg.Enum.mode import Mode
 
-
-
-def get_events() -> list[e.Event]:
+def get_events(mode:Mode=Mode.default) -> list[e.Event]:
+    # 如果是默认模式
+    if mode == Mode.default:
+        from_block = load_from_block()
+    elif mode == Mode.init:
+        # 这个是初始化模式
+        from_block = FROM_BLOCK
     url = ("https://api-sepolia.etherscan.io/api"
               "?module=logs"
               "&action=getLogs"
-              f"&fromBlock={FROM_BLOCK}"
+              f"&fromBlock={from_block}"
               "&toBlock=latest"
               f"&address={CONTRACT_ADDRESS}"
               f"&topic0={TOPIC_0}"
@@ -21,6 +27,8 @@ def get_events() -> list[e.Event]:
     for data in data_list:
         event = e.dict_to_event(data)
         event_list.append(event)
+    # 最后一个event的blockNumber
+    save_from_block(event_list[-1].blockNumber)
     return event_list
 
 
